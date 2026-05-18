9d0449a3b27c31361ea36fef48af30288d7aa3e8
fanyujie <2576420317@qq.com>
2026-05-18 11:18:24 +0800

    增加前端

--- DemoWebApi/obj/Release/net8.0/DemoWebApi.AssemblyInfo.cs
+++ DemoWebApi/obj/Release/net8.0/DemoWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796bd806be049821d52f6eeac7a67d7ea84f955f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd2acf65e0799e7e416da96ac1ccb4b76dfd20a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
